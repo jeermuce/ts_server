160e09682a8e6fee358ddb7f3619673448373b13
jeermuce <ernesto_mc.dev@proton.me>
2024-11-04 23:33:03 -0600

    broadcast

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
-import WebSocket from 'ws';
-const server = new WebSocket.Server({ port: 3001});
-console.log('WebSocket server started on ws://localhost:3001');
-server.on('connection', ws => {
-    ws.on('message', message => {
+import WebSocket from "ws";
+const server = new WebSocket.Server({ port: 3001 });
+console.log("WebSocket server started on ws://localhost:3001");
+
+let counter = 0;
+server.on("connection", (ws) => {
+    ws.on("message", (message) => {
         console.log(`Received message => ${message}`);
-        ws.send('Hello! Response from server!');
+        counter++;
+        ws.send(`Message received! Counter: ${counter}`);
+        broadcastCounter();
     });
-    ws.send('Hello! Message from server!');
+    ws.send("Hello! Message from server!");
 });
+
+function broadcastCounter() {
+    for (const client of server.clients) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(`Counter: ${counter}`);
+        }
+    }
+}
