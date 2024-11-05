69c80529230eef572c373a8dfc5979a73d40bdde
jeermuce <ernesto_mc.dev@proton.me>
2024-11-04 23:41:00 -0600

    fx

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
 import WebSocket from "ws";
+
 const server = new WebSocket.Server({ port: 3001 });
 console.log("WebSocket server started on ws://localhost:3001");
 
 let counter = 0;
+
 server.on("connection", (ws) => {
     ws.on("message", (message) => {
         console.log(`Received message => ${message}`);
         counter++;
-        ws.send(`Message received! Counter: ${counter}`);
-        broadcastCounter();
+
+        // Broadcast the counter value to all connected clients
+        server.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(`Current counter: ${counter}`);
+            }
+        });
     });
+
     ws.send("Hello! Message from server!");
 });
-
-function broadcastCounter() {
-    for (const client of server.clients) {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(`Counter: ${counter}`);
-        }
-    }
-}
